6fa2038144c46515ed92ab08b634b7c85c09d19b
BrianX340 <brianx340@gmail.com>
2024-09-30 21:18:31 -0300

    cg

---
+++ _next/static/g6Y1KCQoPeusLDfuEY9GA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/411-7a4d383e6a81a34c.js","static/chunks/pages/index-6a449f3caa725241.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
